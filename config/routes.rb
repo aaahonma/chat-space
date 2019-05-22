750d3d6f21ca3d33601a7474403c51654c4d2d4c
naoto_honma <naoto.honma@di-v.co.jp>
2019-05-22 15:40:06 +0900

    prefix_resetting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'groups/new'
-
-  get 'users/new'
-
-  get 'messages/index'
+  resources :users
+  resources :groups do
+    resources :messages
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
 end
-
