c5c761cb80c87618edf8a6fcdaf90f721e3d008d
naoto_honma <naoto.honma@di-v.co.jp>
2019-05-23 16:13:13 +0900

    commit1

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-
-  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_groups_on_name", using: :btree
-  end
-
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "message",    limit: 65535
-    t.string   "image"
-    t.integer  "user_id",                  null: false
-    t.integer  "group_id",                 null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
-  end
+ActiveRecord::Schema.define(version: 20190523052718) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",                   null: false
-    t.string   "email",                      null: false
+    t.string   "nickname",                                          null: false
+    t.string   "name",                                              null: false
+    t.string   "email",                                default: "", null: false
     t.string   "icon_image"
-    t.text     "introduction", limit: 65535
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "name",                       null: false
+    t.text     "introduction",           limit: 65535
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "messages", "groups"
-  add_foreign_key "messages", "users"
 end
