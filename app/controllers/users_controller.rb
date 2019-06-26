e483598028587127777ec005ee58277dd96cf78b
naoto_honma <naoto.honma@di-v.co.jp>
2019-06-26 22:35:29 +0900

    commit_2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
-        @users = User.where('nickname LIKE(?)', "%#{params[:nickname]}%").where.not(nickname: current_user.nickname).limit(10)
+        @users = User.where('nickname LIKE(?)', "%#{params[:nickname]}%").where.not(id: current_user.id)
         # .where.not(nickname: current_user.nickname)= 自分のユーザーデータのみDBより取得除外する
         respond_to do |users|
             users.html
